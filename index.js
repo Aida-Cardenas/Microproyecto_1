c5fe77124541f54ca8d150415084e35fbce60a6b
Aida Cardenas <aida.cardenas@correo.unimet.edu.ve>
2025-02-11 20:42:24 -0400

    IT WORKS OMFG

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const topLeft = document.querySelector('.top-left-panel');
-const topRight = document.querySelector('.top-right-panel');
-const bottomLeft = document.querySelector('.bottom-left-panel');
-const bottomRight = document.querySelector('.bottom-right-panel');
+const topLeft = document.getElementById('yellow');
+const topRight = document.getElementById('green');
+const bottomLeft = document.getElementById('blue');
+const bottomRight = document.getElementById('red');
 
 // Sacar un panel random
 const getRandomPanel = () => {
@@ -14,7 +14,6 @@ const getRandomPanel = () => {
     return panels[parseInt(Math.random() * panels.length)];
 }
 
-// Constantes de la secuencia
 const sequence = [getRandomPanel()];
 let sequenceToGuess = [...sequence];
 
@@ -25,22 +24,23 @@ const flash = (panel) => {
             panel.className = panel.className.replace(' active','');
             setTimeout(() => {
                 resolve();
-            }, 250);
+            }, 700);
         }, 700);
     });
 };
 
-let canClick = false;
 const panelClicked = (panel) => {
-    if (!canClick) return;
-    const expectedPanel = sequenceToGuess.shift();
+    flash(panel);
+    const expectedPanel = sequenceToGuess.shift(); //panel que deberia clickear
     // lógica del juego
-    if (expectedPanel === panel) {
+    if (expectedPanel.id === panel.id) {
         if (sequenceToGuess.length === 0) {
-            // nueva ronda
-            sequence.push(getRandomPanel());
-            sequenceToGuess = [...sequence];
-            startFlashing();
+            // nueva ronda después de 2 segundos
+            setTimeout(() => {
+                sequence.push(getRandomPanel());
+                sequenceToGuess = [...sequence];
+                startFlashing();
+            }, 2000); // Espera entre cada secuencia (dos segundos)
         }
     } else {
         // perdiste
